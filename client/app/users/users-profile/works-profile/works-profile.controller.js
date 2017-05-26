3cfccdb76f8e60488acee34987e359a4a2431084
Kevin Hincapie <kvn.hincapie.t@gmail.com>
2017-05-26 14:19:33 -0500

    Worked on editing user data

--- client/app/users/users-profile/works-profile/works-profile.controller.js
+++ client/app/users/users-profile/works-profile/works-profile.controller.js
@@ -2,26 +2,59 @@
 (function(){
 
   class WorksProfileComponent {
-    constructor($auth,userService) {
-      this.message = 'Hello';
+    constructor($auth,usersService,citiesService) {
+      this.usersService =usersService;
       this.$auth=$auth;
       this.userId;
+      this.gender = ["Masculino", "Femenino"];
+      this.citiesService=citiesService;
     }
+
 
     $onInit(){
 
-   this.userId=this.$auth.getPayload().sub;
+      this.citiesService.query().$promise
+        .then(response=>{
+
+          this.cities=response;
+        }).catch(err=>{
+
+          console.log("city error",err);
+        });
+
+       this.userId=this.$auth.getPayload().sub;
+
+      this.usersService.get({id:this.userId}).$promise
+        .then(response=>{
+          this.user=response;
+          console.log("usuario",this.user);
+        }).catch(err=>{
+
+          console.log("error",err);
+        });
 
    console.log("userId",this.userId);
 
     }
+
+      editUser(){
+        this.usersService.update(this.user).$promise
+          .then(response=>{
+            console.log("usuario",this.user);
+          }).catch(err=>{
+
+            console.log("error",err);
+          });
+      }
+
   }
 
-
+WorksProfileComponent.$inject=['$auth','usersService','citiesService'];
 angular.module('startUpApp')
   .component('worksProfile', {
     templateUrl: 'app/users/users-profile/works-profile/works-profile.html',
-    controller: WorksProfileComponent
+    controller: WorksProfileComponent,
+    controllerAs:'vm'
   });
 
 })();
--- client/app/users/users.service.js
+++ client/app/users/users.service.js
@@ -6,9 +6,31 @@
   usersService.inject = ["$resource", "API"];
 
   function usersService($resource, API) {
-    return $resource(API + "/api/users/:id", {
-      id: '@id'
+    return $resource(API + "/api/users/:id",null, {
+      'get': {
+        method: 'GET',
+        params: {
+          id: '@id'
+        }
+      },
+      'save': {
+        method: 'POST'
+      },
+      'query': {
+        method: 'GET',
+        isArray: true
+      },
+      'remove': {
+        method: 'DELETE',
+        params: {
+          id: '@id'
+        }
+      },
+      'update': {
+        method: 'PUT'
+      }
     })
+
   }
 })();
 // el inject me sirve para injectar una libreria de angular y las incluye por medio de cadenas de texto en la cual las va a comprimir.
