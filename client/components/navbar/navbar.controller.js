b4fb5c262769f3ba1cf612825451007599be3f75
Kevin Hincapie <kvn.hincapie.t@gmail.com>
2017-05-28 17:19:30 -0500

    showing logged username

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,15 +2,27 @@
 
 class NavbarController {
 
-  constructor($auth,AuthService){
+  constructor($auth,AuthService,usersService){
 
 this.$auth=$auth;
 this.AuthService=AuthService;
+this.usersService=usersService;
+this.userId;
 
   }
 
   $onInit(){
 
+    this.userId=this.$auth.getPayload().sub;
+
+   this.usersService.get({id:this.userId}).$promise
+     .then(response=>{
+       this.user=response;
+       console.log("usuario",this.user);
+     }).catch(err=>{
+
+       console.log("error",err);
+     });
 
 
   }
@@ -29,6 +41,6 @@ this.AuthService=AuthService;
 
 
 }
-NavbarController.$inject=['$auth','AuthService'];
+NavbarController.$inject=['$auth','AuthService','usersService'];
 angular.module('startUpApp')
   .controller('NavbarController', NavbarController);
