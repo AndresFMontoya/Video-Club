958db7ba835c5548b07034e3c7440f58722077f0
Kevin Hincapie <kvn.hincapie.t@gmail.com>
2017-06-02 11:08:24 -0500

    slider fixed

--- client/components/slider/slider.controller.js
+++ client/components/slider/slider.controller.js
@@ -12,6 +12,30 @@ $onInit(){
 
 
 }
+
+carousel_left() {
+
+
+		if (CurrentSlide > 0) {
+
+    		Direction = 0;
+				$("#slideshow_carousel").carousel( "prev" );
+		}
+	}
+
+
+  carousel_right() {
+
+
+		if ((CurrentSlide  + 1) < TotalSlides) {
+
+  		Direction = 1;
+			$("#slideshow_carousel").carousel( "next" );
+		} else {
+			progressClick(0);
+		}
+
+	}
 }
 SliderComponent.$inject=['AuthService'];
 angular.module('startUpApp')
