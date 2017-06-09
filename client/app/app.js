9b2111700f2c55da0c2cec08e141f67957b2c5ad
AndresMontoya2 <andres19298@gmail.com>
2017-06-09 07:00:19 -0500

    Responsive Profile

--- client/app/app.js
+++ client/app/app.js
@@ -18,31 +18,32 @@ angular.module('startUpApp', [
     $authProvider.tokenName='token';
     $authProvider.tokenPrefix='startUpApp';
     $locationProvider.html5Mode(true);
-  }).run(function($state,$rootScope, $auth) {
-     $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
-
-      if ($auth.isAuthenticated()) {
-        if (($auth.getPayload().roles.indexOf("TRAB") !== -1 || $auth.getPayload().roles.indexOf("FERR") !== -1) && (toState.name === "users-update" || toState.name === "users-create")) {
-          e.preventDefault();
-          $state.go('main');
-        } else if($auth.getPayload().roles.indexOf("TRAB")!== -1 && toState.name === "ironmongery-profile") {
-          e.preventDefault();
-          $state.go('works-profile');
-        } else if($auth.getPayload().roles.indexOf("FERR")!== -1 && toState.name === "works-profile") {
-          e.preventDefault();
-          $state.go('ironmongery-profile');
-        }
-      }else{
-        if (!$auth.isAuthenticated() &&toState.name === "users-update") {
-          e.preventDefault();
-          $state.go('not-found');
-        } else if(!$auth.isAuthenticated() && toState.name === "ironmongery-profile") {
-          e.preventDefault();
-          $state.go('not-found');
-        } else if(!$auth.isAuthenticated() && toState.name === "works-profile") {
-          e.preventDefault();
-          $state.go('not-found');
-        }
-      }
-     });
-});
+  });
+//   .run(function($state,$rootScope, $auth) {
+//      $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+//
+//       if ($auth.isAuthenticated()) {
+//         if (($auth.getPayload().roles.indexOf("TRAB") !== -1 || $auth.getPayload().roles.indexOf("FERR") !== -1) && (toState.name === "users-update" || toState.name === "users-create")) {
+//           e.preventDefault();
+//           $state.go('main');
+//         } else if($auth.getPayload().roles.indexOf("TRAB")!== -1 && toState.name === "ironmongery-profile") {
+//           e.preventDefault();
+//           $state.go('works-profile');
+//         } else if($auth.getPayload().roles.indexOf("FERR")!== -1 && toState.name === "works-profile") {
+//           e.preventDefault();
+//           $state.go('ironmongery-profile');
+//         }
+//       }else{
+//         if (!$auth.isAuthenticated() &&toState.name === "users-update") {
+//           e.preventDefault();
+//           $state.go('not-found');
+//         } else if(!$auth.isAuthenticated() && toState.name === "ironmongery-profile") {
+//           e.preventDefault();
+//           $state.go('not-found');
+//         } else if(!$auth.isAuthenticated() && toState.name === "works-profile") {
+//           e.preventDefault();
+//           $state.go('not-found');
+//         }
+//       }
+//      });
+// });
