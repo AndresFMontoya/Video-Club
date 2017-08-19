37f173e8a174f13340da37e665dc670b4e94c816
kevin hincapie <kvn.hincapi.t@gmail.com>
2017-08-19 01:47:26 -0500

    creado el autocomplete de ciudades

--- client/app/cities/cities-create/cities-create.controller.js
+++ client/app/cities/cities-create/cities-create.controller.js
@@ -2,12 +2,26 @@
   'use strict';
 
   class CitiesCreateComponent {
-    constructor() {
+    constructor(citiesService) {
+      this.citiesService=citiesService;
     }
     $onInit() {
     }
-  }
 
+    query(searchText){
+    return this.citiesService.getCiudades({name:searchText}).$promise
+     .then(function(data) {
+        // Map the response object to the data object.
+        return data;
+      })
+
+      }
+
+      op(element){
+        console.log("element",element);
+      }
+  }
+CitiesCreateComponent.$inject=['citiesService'];
   angular.module('startUpApp')
     .component('citiesCreate', {
       templateUrl: "app/cities/cities-create/cities-create.html",
